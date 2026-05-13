fa733489a0e32a670f44ad2c8b5149d00caaf581
unknown <rafaosbcarvalho@gmail.com>
2026-05-13 16:18:08 -0300

    Testando hiperparametros, e vendo se discretizacao ajuda

--- trabalho/modelocerto.py
+++ trabalho/modelocerto.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn import tree  # Arvore de decisão e plot tree
 from sklearn.metrics import accuracy_score   # Acurácia
 from sklearn.preprocessing import OrdinalEncoder,OneHotEncoder, MinMaxScaler,KBinsDiscretizer,FunctionTransformer  # Transformar coluna ordinária
-from sklearn.model_selection import train_test_split,GridSearchCV  # Separar a parte de teste
+from sklearn.model_selection import train_test_split,GridSearchCV,StratifiedKFold  # Separar a parte de teste
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay  # Matriz de confusão
 import matplotlib.pyplot as plt  # Plot na tabela
 from sklearn.impute import KNNImputer
@@ -25,7 +25,6 @@ df = pd.read_csv("class_german_credit.csv")
 binary_cols = ['Sex']
 
 housing_col = ['Housing']
-
 
 saving_col = ['Saving accounts']
 
@@ -58,7 +57,6 @@ print(df.head())
 X = df.drop(columns='Risk')
 
 preprocessor = ColumnTransformer([
-
     # Sex -> binary
     ('sex',OrdinalEncoder(categories=[['female', 'male']]), binary_cols),
 
@@ -68,48 +66,29 @@ preprocessor = ColumnTransformer([
         housing_col
     ),
 
-
     # Saving accounts
-    (
-        'saving',
-        Pipeline([
-            (
-                'encoder',
-                OrdinalEncoder(
+    ('saving',OrdinalEncoder(
                     handle_unknown='use_encoded_value',
                     unknown_value=np.nan,
-                    categories=[[
-                        'little',
-                        'moderate',
-                        'rich',
-                        'quite rich'
-                    ]]
-                )
-            ),
-        ]),
+                    categories=[['little','moderate','rich','quite rich']]),
         saving_col
     ),
 
     # Checking account
     (
-        'checking',
-        Pipeline([
-            (
-                'encoder',
-                OrdinalEncoder(
+        'checking',OrdinalEncoder(
                     handle_unknown='use_encoded_value',
                     unknown_value=np.nan,
-                    categories=[[
-                        'little',
-                        'moderate',
-                        'rich'
-                    ]]
-                )
-            ),
-        ]),
+                    categories=[['little','moderate','rich']]),
         checking_col
-    )
+    ),
 
+    #Discretization amounts
+    ('age_bins',KBinsDiscretizer(encode='ordinal',quantile_method='linear'), ['Age']),
+
+    ('duration_bins',KBinsDiscretizer(encode='ordinal',quantile_method='linear'),['Duration']),
+
+    ('credit_bins',KBinsDiscretizer(encode='ordinal',quantile_method='linear'),['Credit amount'])
 ], remainder='passthrough')
 
 # =========================
@@ -134,34 +113,48 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42,
     stratify=y
 )
-
-
 X_train_processed = pipe[:-1].fit_transform(X_train, y_train)
 X_test_processed = pipe[:-1].transform(X_test)
 
 feature_names = pipe.named_steps['preprocessing'].get_feature_names_out()
 
-X_train_processed = pd.DataFrame(
-    X_train_processed,
-    columns=feature_names,
-    index=X_train.index
-)
+X_train_processed = pd.DataFrame(X_train_processed,columns=feature_names,index=X_train.index)
 
-X_test_processed = pd.DataFrame(
-    X_test_processed,
-    columns=feature_names,
-    index=X_test.index
-)
+X_test_processed = pd.DataFrame(X_test_processed,columns=feature_names,index=X_test.index)
+
 print(X_train_processed.head())
 print(X_train_processed['saving__Saving accounts'].value_counts())
 print(X_train_processed['checking__Checking account'].value_counts()) #isso eh apenas para visualizar o que o kNN fez
 print(X_train_processed.info())
 
-pipe.fit(X_train, y_train)
-y_pred = pipe.predict(X_test)
+param_grid = {
+    'preprocessing__age_bins__n_bins': [2,3,4,5,6],
+    'preprocessing__duration_bins__n_bins': [2,3,4,5,6],
+    'preprocessing__credit_bins__n_bins': [2,3,4,5,6],
+
+    'preprocessing__age_bins__strategy': ['uniform', 'quantile'],
+    'preprocessing__duration_bins__strategy': ['uniform', 'quantile'],
+    'preprocessing__credit_bins__strategy': ['uniform', 'quantile'],
+    'model__max_depth': [3,5,7,None]
+}
+cv_strategy = StratifiedKFold(n_splits=5) #classes desbalanceadas tem que manter stratified
+grid = GridSearchCV(
+    pipe,
+    param_grid,
+    scoring='accuracy',
+    cv=cv_strategy
+)
+
+grid.fit(X_train, y_train)
+
+print(grid.best_params_)
+print(grid.best_score_)
+best_model = grid.best_estimator_
+
+y_pred = best_model.predict(X_test)
 
 acuracia = accuracy_score(y_test, y_pred)
-print(f"{acuracia*100:.2f}%")
+print(f"{acuracia*100:.2f}% best model accuracy")
 best_cm = confusion_matrix(y_test, y_pred)
 disp = ConfusionMatrixDisplay(confusion_matrix=best_cm)
 disp.plot(cmap='Blues')
